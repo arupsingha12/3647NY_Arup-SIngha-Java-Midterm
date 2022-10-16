0f7b5d219f88bd9560f5ddc494c7f48944237bab
arupsingha12 <a.ratan004@gmail.com>
2022-10-16 03:57:44 -0400

    worked on palindrome

--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -12,42 +12,30 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
+        System.out.println(isPalindrome("madam"));
+
     }
 
     // Implement here
+    static boolean isPalindrome (String str) {
 
-        /** INSTRUCTIONS
-         * A palindrome is a word that can be reversed, and still remain the same.
-         *
-         * Example: MOM, DAD, MADAM, RACECAR
-         *
-         * Create a method to check if any given String is a palindrome or not.
-         */
+        boolean isPalindrome = false;
 
-            System.out.println(isPalindrome("racecar"));
+        str.toLowerCase();
 
-        }
-        // Implement here
+        char[] charArray = str.toCharArray();
+        int stringLength = charArray.length;
 
-        public static boolean isPalindrome (String str) {
-
-            boolean isPalindrome = false;
-
-            str.toLowerCase();
-
-            char[] charArray = str.toCharArray();
-            int stringLength = charArray.length;
-
-            for (int i = 0; i < stringLength / 2; i++) {
-                if (charArray[i] == charArray[stringLength - 1 - i]) {
-                    isPalindrome = true;
-                }
-                return isPalindrome;
+        for (int i = 0; i < stringLength / 2; i++) {
+            if (charArray[i] == charArray[stringLength - 1 - i]) {
+                isPalindrome = true;
             }
-
             return isPalindrome;
-
         }
+
+        return isPalindrome;
+
+    }
 
 
 }
