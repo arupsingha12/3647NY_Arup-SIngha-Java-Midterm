6e06d4b57e770cff6e91641198036e776b129646
arupsingha12 <a.ratan004@gmail.com>
2022-10-16 04:12:38 -0400

    worked on factorial

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -10,7 +10,17 @@ public class LowestNumber {
 
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
+        System.out.println(findLowestNumber(array));
 
+
+    }
+
+    public static int findLowestNumber (int [] array) {
+
+        Arrays.sort(array);
+
+        return array[0];
+    }
 
     }
 
